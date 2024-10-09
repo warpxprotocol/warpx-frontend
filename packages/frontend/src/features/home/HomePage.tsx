77fb28d09214db77909e56a88673057bd66d1a13
Junho Yeo <i@junho.io>
2024-10-10 00:20:32 +0900

    Implement `HybridSection`

--- packages/frontend/src/features/home/HomePage.tsx
+++ packages/frontend/src/features/home/HomePage.tsx
@@ -2,11 +2,13 @@ import styled from '@emotion/styled';
 import { NextPage } from 'next';
 
 import { HeroSection } from '@/features/home/sections/HeroSection';
+import { HybridSection } from '@/features/home/sections/HybridSection';
 
 const Home: NextPage = () => {
   return (
     <Wrapper>
       <HeroSection />
+      <HybridSection />
     </Wrapper>
   );
 };
